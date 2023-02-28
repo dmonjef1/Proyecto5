abfa7bb0aef568f7be1934a700ca1dfe62db17e1
dylan <dylanmonje0603@gmail.com>
2023-02-27 23:43:42 -0300

    final

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,53 +1,24 @@
 
-<<<<<<< HEAD
+
 import {BrowserRouter, Route , Routes} from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen.js';
 import ProductScreen from "./screens/ProductScreen.js";
-import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
-import {LinkContainer} from 'react-router-bootstrap';
-=======
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import HomeScreen from './screens/HomeScreen.js';
-import ProductScreen from "./screens/ProductScreen.js";
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
+;
 //import Navbar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/Container';
 //import {LinkContainer} from 'react-router-bootstrap';
 //import Button from 'react-bootstrap/Button';
 //import Nav from 'react-bootstrap/Nav';
 //import { Link } from 'react-router-dom';
-import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
->>>>>>> 932055e5e97c757a2d61685392ffa583b4f50884
+
+
 
 
 
 function App() {
   return (
     <BrowserRouter>
-<<<<<<< HEAD
-    <div className="d-flex flex-column site-container">
-      <header>
-        <Navbar>
-          <Container>
-        <LinkContainer to="/">
-          <Navbar.Brand>Proyecto Ecommerce</Navbar.Brand>
-        </LinkContainer>
-        </Container>
-        </Navbar>
-      </header>
-      <main>
-        <Container>
-        <Routes>
-          <Route path="/products/:slug" element={<ProductScreen/>} />
-          <Route path="/" element={<HomeScreen/>}/>
-        </Routes>
-        </Container>     
-      </main>
-      <footer>
-        <div className="text-center">Todo los derechos reservados por DMONJE/SCORTESC</div>
-      </footer>
-    </div>
-=======
       <div className="d-flex flex-column site-container">
         <header>
           <Navbar bg="#f0c040" expand="lg">
@@ -88,15 +59,14 @@ function App() {
               Todos los derechos reservados &copy; DMONJE/SCORTESC
             </p>
             <div style={{ marginTop: "10px" }}>
-              <a href="#">Política de privacidad</a>
+              <a href="/">Política de privacidad</a>
               <span style={{ margin: "0 10px" }}>|</span>
-              <a href="#">Términos y condiciones</a>
+              <a href="/">Términos y condiciones</a>
             </div>
           </div>
         </footer>
 
       </div>
->>>>>>> 932055e5e97c757a2d61685392ffa583b4f50884
     </BrowserRouter>
   );
 }
